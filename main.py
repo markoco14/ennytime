df14801fc60306055635959a4413fbbfbfd519b9
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-02 18:32:20 +0800

    add fake schedule data
    implement shift on each day rendering logic on the frontend
    
    will likely move logic to backend

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+
+
 import time_service
 
 app = FastAPI()
@@ -9,6 +11,31 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+SHIFT_TYPES = ["N","D","W"]
+
+SHIFTS = [
+	{
+		"type": "N",
+		"date": 1
+	},
+	{
+		"type": "N",
+		"date": 11
+	},
+	{
+		"type": "D",
+		"date": 21
+	},
+	{
+		"type": "N",
+		"date": 25
+	},
+	{
+		"type": "W",
+		"date": 29
+	},
+]
 
 MONTH_CALENDAR = time_service.get_month_calendar(2024, 2)
 @app.get("/", response_class=HTMLResponse)
@@ -18,7 +45,8 @@ def index(request: Request):
 	context = {
 		"request": request,
 		"days_of_week": DAYS_OF_WEEK,
-		"month_calendar": MONTH_CALENDAR
+		"month_calendar": MONTH_CALENDAR,
+		"shifts": SHIFTS,
 	}
 
 	return templates.TemplateResponse(
