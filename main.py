ad9018a5533cd0d3788afeb552d9c12ed86ef4f8
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-02 17:50:27 +0800

    create DAYS_OF_WEEK and calendar header

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
 	"""Index page"""
-	return templates.TemplateResponse(request=request, name="index.html", context={"request": request})
+
+	context = {
+		"request": request,
+		"days_of_week": DAYS_OF_WEEK
+	}
+	
+	return templates.TemplateResponse(
+		request=request, 
+		name="index.html",
+		context=context
+		)
