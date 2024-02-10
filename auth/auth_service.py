43f9d934a303a2e9b95f7aff31898cef9b5796e3
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-10 17:58:54 +0800

    add verify_password function to auth_service

--- auth/auth_service.py
+++ auth/auth_service.py
@@ -8,3 +8,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def get_password_hash(password):
     """returns hashed password"""
     return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    """returns True if password is correct, False if not"""
+    return pwd_context.verify(plain_password, hashed_password)
