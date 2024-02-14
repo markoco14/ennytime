ab3b6e7d901c6ba64520b1261655f640612127a1
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-14 11:53:16 +0800

    add patch user function to user_repository

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -1,8 +1,17 @@
 """CRUD functions for users table"""
 
 from memory_db import USERS
+from schemas import User
 
 
 def list_users():
     """ Returns a list of users """
     return list(USERS.values())
+
+def patch_user(current_user: User, display_name: str = None):
+    """ Updates a user """
+    if display_name is not None:
+        current_user.display_name = display_name
+
+    USERS.update({current_user.email: current_user})
+    
