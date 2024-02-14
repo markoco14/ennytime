7072580b6b3cc0426d83a8618ae48d5ea7cbd577
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-14 16:57:48 +0800

    create a test_db and user_repository_tests.py
    not real tests, just want to try the repository functions from the file to see if they work

---
+++ repositories/user_repository_tests.py
@@ -0,0 +1,33 @@
+# """CRUD functions for users table"""
+# from sqlalchemy.orm import Session
+# from sqlalchemy.exc import IntegrityError
+# from core.memory_db import USERS
+# from core.database import test_db
+# import schemas
+# from repositories import user_repository
+
+
+
+# from models.user_model import DBUser
+
+
+
+# TESTS
+# import test_db ie; from core.database import test_db
+# and use it directly as the db variable ie; db=test_db
+    
+# try:
+#     test_create_user = schemas.CreateUserHashed(
+#         email="youremail@email.com",
+#         hashed_password="fakehashedsecrethashed"
+#     )
+#     user_repository.create_user(db=test_db, user=test_create_user)
+#     test_db_user = user_repository.get_user_by_email(db=test_db, email="myemail@email.com")
+#     print(test_db_user)
+# except IntegrityError as e:
+#     print(e.orig)
+    
+# finally:
+#     test_db.close()
+
+
