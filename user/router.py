83565e1f2fd9eb6f0de8b3d3d4b432f2304951df
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-14 11:20:54 +0800

    set up view/edit display name toggle

--- user/router.py
+++ user/router.py
@@ -38,3 +38,58 @@ def get_profile_page(request: Request):
         name="profile.html",
         context=context
         )
+
+@router.get("/contact/{user_id}", response_class=HTMLResponse | Response)
+def get_display_name_widget(request: Request, user_id: int):
+	if not auth_service.get_session_cookie(request.cookies):
+		return templates.TemplateResponse(
+			request=request,
+			name="landing-page.html",
+			headers={"HX-Redirect": "/"},
+		)
+
+	session_data: Session = auth_service.get_session_data(request.cookies.get("session-id"))
+	
+	current_user: User = auth_service.get_current_user(user_id=session_data.user_id)
+
+	if current_user.id != user_id:
+		return Response(status_code=403)
+	
+	context = {
+		"request": request,
+		"user": current_user,
+	}
+	return templates.TemplateResponse(
+		request=request,
+		name="/contact/display-name.html",
+		context=context
+		)
+
+@router.get("/contact/{user_id}/edit", response_class=HTMLResponse | Response)
+def get_edit_display_name_widget(request: Request, user_id: int):
+	"""Edit contact page"""
+	print(user_id)
+	if not auth_service.get_session_cookie(request.cookies):
+		return templates.TemplateResponse(
+			request=request,
+			name="landing-page.html",
+			headers={"HX-Redirect": "/"},
+		)
+
+	session_data: Session = auth_service.get_session_data(request.cookies.get("session-id"))
+	
+	current_user: User = auth_service.get_current_user(user_id=session_data.user_id)
+
+	if current_user.id != user_id:
+		return Response(status_code=403)
+
+	context = {
+		"request": request,
+		"user": current_user,
+	}
+
+	return templates.TemplateResponse(
+		request=request,
+		name="/contact/display-name-edit.html",
+		context=context
+		)
