5f5ad118f3121f584bce225483ddc6f962ee5d5f
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-14 22:53:43 +0800

    move register shift type logic to shift_type_router

---
+++ routers/shift_type_router.py
@@ -0,0 +1,78 @@
+
+from typing import Annotated
+from fastapi import APIRouter, Depends, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from sqlalchemy.exc import IntegrityError
+
+from auth import auth_service
+from core.database import get_db
+import core.memory_db as memory_db
+
+from repositories import shift_type_repository
+from schemas import Session
+import schemas
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@router.post("/register-shift-type", response_class=HTMLResponse)
+def register_shift_type(
+    request: Request,
+    shift_type: Annotated[str, Form()],
+    db: Annotated[Session, Depends(get_db)],):
+    """Register shift type"""
+    if not auth_service.get_session_cookie(request.cookies):
+        return templates.TemplateResponse(
+            request=request,
+            name="landing-page.html",
+            headers={"HX-Redirect": "/"},
+        )
+    # get the session data
+    session_data: Session = auth_service.get_session_data(
+        db=db,
+        session_token=request.cookies.get("session-id")
+        )
+    # get the current user
+    current_user: schemas.AppUser = auth_service.get_current_user(
+        db=db,
+        user_id=session_data.user_id
+        )
+    
+    # get new shift type data ready
+    new_shift_type = schemas.CreateShiftType(
+        type=shift_type,
+        user_id=current_user.id
+    )
+
+    # create new shift type or return an error
+    try:
+        shift_type_repository.create_shift_type(
+            db=db,
+            shift_type=new_shift_type
+            )
+    except IntegrityError:
+        return templates.TemplateResponse(
+            request=request,
+            name="shifts/shift-list.html",
+            context={"error": "Something went wrong."}
+        )
+
+    # get the new shift type list
+    shift_types = shift_type_repository.list_user_shift_types(
+        db=db,
+        user_id=current_user.id
+        )
+    
+    context={
+        "request": request,
+        "shift_types": shift_types,
+          }
+
+    return templates.TemplateResponse(
+        request=request,
+        name="shifts/shift-list.html", # change to list template
+        context=context
+    )
