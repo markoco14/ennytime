d32ee79960b353920c46cc8c091e6169521ba368
Mark O'Connor <mark.oconnor14@gmail.com>
2024-02-19 17:31:46 +0800

    add get_share_by_owner_id to share repository

--- app/repositories/share_repository.py
+++ app/repositories/share_repository.py
@@ -3,6 +3,12 @@ from sqlalchemy.orm import Session
 from app.models.share_model import DbShare
 from app import schemas
 
+def get_share_by_owner_id(db: Session, user_id: int):
+    """Get a share by user id"""
+    db_share = db.query(DbShare).filter(
+        DbShare.owner_id == user_id).first()
+    return db_share
+
 def get_share_by_guest_id(db: Session, guest_id: int):
     """Get a share by guest id"""
     db_share = db.query(DbShare).filter(
