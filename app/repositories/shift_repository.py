e92a8bf61b1e73a47317733f497504076fe1efa0
Mark O'Connor <mark.oconnor14@gmail.com>
2024-03-02 14:33:13 +0800

    create get_user_shift_details method to shift_repository
    gets shifts by user_id and their related shift_type details
    to improve performance and simplifying calendar requests

--- app/repositories/shift_repository.py
+++ app/repositories/shift_repository.py
@@ -1,6 +1,7 @@
 """
 Functions for retrieving data from any table in the database
 """
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 from app.models.db_shift import DbShift
 from app.schemas import schemas
@@ -15,9 +16,21 @@ def create_shift(db: Session, shift: schemas.CreateShift):
 	
 	return db_shift_type
 
-def get_user_shifts(db: Session, user_id: int) -> list[schemas.AppShift]:
+def get_user_shifts(db: Session, user_id: int):
 	""" Get shift by date """
-	return db.query(DbShift).filter(DbShift.user_id == user_id).order_by(DbShift.date)
+	return db.query(DbShift).filter(DbShift.user_id == user_id).order_by(DbShift.date).all()
+
+def get_user_shifts_details(db: Session, user_id: int):
+	""" Get shifts by User with shift type details """
+	query = text("""
+        SELECT etime_shifts.*, etime_shift_types.type as type_name
+        FROM etime_shifts
+        JOIN etime_shift_types ON etime_shifts.type_id = etime_shift_types.id
+        WHERE etime_shifts.user_id = :user_id
+        ORDER BY etime_shifts.date
+    """)
+	result = db.execute(query, {"user_id": user_id})
+	return result.fetchall()
 
 
 def delete_user_shift(db: Session, shift_id: int):
