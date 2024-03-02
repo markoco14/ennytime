ba07f8d13e365fc3e7d450b8c55cc1c0bb11261b
Mark O'Connor <mark.oconnor14@gmail.com>
2024-03-02 13:24:27 +0800

    formatting

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ templates = Jinja2Templates(directory="templates")
 
 handler = Mangum(app)
 
+
 @app.get("/", response_class=HTMLResponse)
 def index(
     request: Request,
@@ -35,7 +36,7 @@ def index(
     db: Annotated[Session, Depends(get_db)],
     month: Optional[int] = None,
     year: Optional[int] = None,
-    ):
+):
     """Index page"""
     current_user = auth_service.get_current_session_user(
         db=db,
@@ -44,41 +45,48 @@ def index(
         response = templates.TemplateResponse(
             request=request,
             name="website/web-home.html"
-            )
+        )
         response.delete_cookie("session-id")
-    
+
         return response
 
     current_month = calendar_service.get_current_month(month)
     current_year = calendar_service.get_current_year(year)
 
-    month_calendar = calendar_service.get_month_calendar(current_year, current_month)
-    
-    month_calendar_dict = dict((str(day), {"date": str(day), "day_number": day.day, "month_number": day.month, "shifts": [], "bae_shifts": []}) for day in month_calendar)
-    
-    db_shifts = shift_repository.get_user_shifts(db=db, user_id=current_user.id)
-    
+    month_calendar = calendar_service.get_month_calendar(
+        current_year, current_month)
+
+    month_calendar_dict = dict((str(day), {"date": str(
+        day), "day_number": day.day, "month_number": day.month, "shifts": [], "bae_shifts": []}) for day in month_calendar)
+
+    db_shifts = shift_repository.get_user_shifts(
+        db=db, user_id=current_user.id)
+
     # get users detailed shift info for matching dates
     for shift in db_shifts:
         shift_date = str(shift.date.date())
         if month_calendar_dict.get(shift_date):
-            db_shift_type = shift_type_repository.get_user_shift_type(db=db, user_id=current_user.id, shift_type_id=shift.type_id)
+            db_shift_type = shift_type_repository.get_user_shift_type(
+                db=db, user_id=current_user.id, shift_type_id=shift.type_id)
             # month_calendar_dict[shift_date]['shift_type_id'] = shift.type_id
             # month_calendar_dict[shift_date]['shift_type'] = db_shift_type
             month_calendar_dict[shift_date]['shifts'].append(db_shift_type)
-        
+
     # just get a single share
     # because a user can only share with 1 person for now
     # and also be shared with 1 person
-    share = share_repository.get_share_by_guest_id(db=db, guest_id=current_user.id)
+    share = share_repository.get_share_by_guest_id(
+        db=db, guest_id=current_user.id)
     if share:
-        bae_shifts = shift_repository.get_user_shifts(db=db, user_id=share.owner_id)
+        bae_shifts = shift_repository.get_user_shifts(
+            db=db, user_id=share.owner_id)
         for shift in bae_shifts:
             shift_date = str(shift.date.date())
             if month_calendar_dict.get(shift_date):
-                db_shift_type = shift_type_repository.get_user_shift_type(db=db, user_id=share.owner_id, shift_type_id=shift.type_id)
-                month_calendar_dict[shift_date]['bae_shifts'].append(db_shift_type)
-
+                db_shift_type = shift_type_repository.get_user_shift_type(
+                    db=db, user_id=share.owner_id, shift_type_id=shift.type_id)
+                month_calendar_dict[shift_date]['bae_shifts'].append(
+                    db_shift_type)
 
     context = {
         "month_number": month,
@@ -94,10 +102,8 @@ def index(
         request=request,
         name="webapp/home/app-home.html",
         context=context,
-        )
-    
-    
-    
+    )
+
     return response
 
 
@@ -107,7 +113,8 @@ def get_signin_page(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="website/signin.html",
-        )
+    )
+
 
 @app.get("/signup", response_class=HTMLResponse)
 def get_signup_page(request: Request):
@@ -123,7 +130,7 @@ def search_users_to_share(
     request: Request,
     db: Annotated[Session, Depends(get_db)],
     search_display_name: Annotated[str, Form()] = ""
-    ):
+):
     """ Returns a list of users that match the search string. """
     current_user = auth_service.get_current_session_user(
         db=db,
@@ -132,22 +139,23 @@ def search_users_to_share(
         response = templates.TemplateResponse(
             request=request,
             name="website/web-home.html"
-            )
+        )
         response.delete_cookie("session-id")
-    
+
         return response
-    
+
     if search_display_name == "":
         return templates.TemplateResponse(
             request=request,
             name="webapp/profile/search-results.html",
             context={"request": request, "matching_users": []}
         )
-    
+
     search_display_name_lower = search_display_name.lower()
 
-    matching_users = user_repository.list_users_by_display_name(db=db, current_user_id=current_user.id, display_name=search_display_name_lower)
-        
+    matching_users = user_repository.list_users_by_display_name(
+        db=db, current_user_id=current_user.id, display_name=search_display_name_lower)
+
     context = {"request": request, "matching_users": matching_users}
 
     return templates.TemplateResponse(
@@ -155,4 +163,3 @@ def search_users_to_share(
         name="webapp/profile/search-results.html",
         context=context
     )
-
