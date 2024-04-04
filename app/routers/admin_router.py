f998f035598f3df3b12e0254a0439241616878bd
Mark O'Connor <mark.oconnor14@gmail.com>
2024-04-04 16:00:00 +0800

    create admin homepage and clean admin pages
    remove development sessions page and clean up users page to show less

--- app/routers/admin_router.py
+++ app/routers/admin_router.py
@@ -9,7 +9,6 @@ from app.auth import auth_service
 from app.core.database import get_db
 
 from app.repositories import user_repository as UserRepository
-from app.repositories import session_repository
 
 router = APIRouter(
     prefix="/admin",
@@ -18,6 +17,39 @@ router = APIRouter(
 )
 templates = Jinja2Templates(directory="templates")
 
+
+@router.get("/", response_class=HTMLResponse)
+def read_admin_home_page(
+    request: Request,
+    db: Annotated[Session, Depends(get_db)],
+):
+    """Returns admin section home page"""
+    if not auth_service.get_session_cookie(request.cookies):
+        return templates.TemplateResponse(
+            request=request,
+            name="website/web-home.html",
+            headers={"HX-Redirect": "/"},
+        )
+
+    current_user = auth_service.get_current_session_user(
+        db=db, cookies=request.cookies)
+
+    if not current_user.is_admin:
+        return templates.TemplateResponse(
+            request=request,
+            name="website/web-home.html",
+            headers={"HX-Redirect": "/"},
+        )
+    
+    context = {
+        "request": request,
+    }
+
+    return templates.TemplateResponse(
+        request=request,
+        name="admin/admin-home.html",
+        context=context
+    )
 
 
 
@@ -45,7 +77,7 @@ def list_users(
 
     
     users = UserRepository.list_users(db=db)
-    headings = ["ID", "Display name", "Email", "Actions"]
+    headings = ["Display name", "Actions"]
     context = {
         "request": request,
         "users": users,
@@ -58,38 +90,38 @@ def list_users(
         context=context
     )
 
-@router.get("/sessions", response_class=HTMLResponse)
-def list_sessions(
-    request: Request,
-    db: Annotated[Session, Depends(get_db)]
-    ):
-    """List sessions"""
-    if not auth_service.get_session_cookie(request.cookies):
-        return templates.TemplateResponse(
-            request=request,
-            name="website/web-home.html",
-            headers={"HX-Redirect": "/"},
-        )
+# @router.get("/sessions", response_class=HTMLResponse)
+# def list_sessions(
+#     request: Request,
+#     db: Annotated[Session, Depends(get_db)]
+#     ):
+#     """List sessions"""
+#     if not auth_service.get_session_cookie(request.cookies):
+#         return templates.TemplateResponse(
+#             request=request,
+#             name="website/web-home.html",
+#             headers={"HX-Redirect": "/"},
+#         )
     
-    current_user = auth_service.get_current_session_user(
-        db=db, cookies=request.cookies)
+#     current_user = auth_service.get_current_session_user(
+#         db=db, cookies=request.cookies)
 
-    if not current_user.is_admin:
-        return templates.TemplateResponse(
-            request=request,
-            name="website/web-home.html",
-            headers={"HX-Redirect": "/"},
-        )
+#     if not current_user.is_admin:
+#         return templates.TemplateResponse(
+#             request=request,
+#             name="website/web-home.html",
+#             headers={"HX-Redirect": "/"},
+#         )
     
-    sessions = session_repository.list_sessions(db=db)
-    headings = ["ID", "Session Token", "User ID", "Expiry date", "Actions"]
-    context = {
-        "request": request,
-    "sessions": sessions,
-    "headings": headings
-    }
-    return templates.TemplateResponse(
-        request=request,
-        name="admin/sessions.html",
-        context=context
-    )
+#     sessions = session_repository.list_sessions(db=db)
+#     headings = ["ID", "Session Token", "User ID", "Expiry date", "Actions"]
+#     context = {
+#         "request": request,
+#     "sessions": sessions,
+#     "headings": headings
+#     }
+#     return templates.TemplateResponse(
+#         request=request,
+#         name="admin/sessions.html",
+#         context=context
+#     )
