3f0e06c05408a2d5e68dd36dd375b31ed7f6c045
Mark O'Connor <mark.oconnor14@gmail.com>
2024-05-18 13:03:44 +0800

    finish basic date/shift list template
    all dates are rendered with shift type buttons
    
    if the current date has a shift, then the corresponding type button will render blue
    
    click to add shift to calendar functionality not set up

--- app/routers/shift_router.py
+++ app/routers/shift_router.py
@@ -1,4 +1,4 @@
-from pprint import pprint
+
 from typing import Annotated
 import datetime
 from fastapi import APIRouter, Depends, Form, Request
@@ -9,10 +9,8 @@ from jinja2_fragments.fastapi import Jinja2Blocks
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 
-
 from app.auth import auth_service
 from app.core.database import get_db
-
 from app.schemas import schemas
 from app.repositories import shift_repository, shift_type_repository, share_repository, user_repository
 from app.services import calendar_service
@@ -40,30 +38,44 @@ def get_add_shifts_page(
         db=db,
         cookies=request.cookies)
 
-    # temporary month and year hard coded
+    # need to handle the case where year and month are not provided
     current_time = datetime.datetime.now()
     if not year:
         year = current_time.year
     if not month:
         month = current_time.month
 
-    # get the month calendar
     month_calendar = calendar_service.get_month_date_list(
         year=year,
         month=month
     )
-    # I think I can make this a date
-    calendar_date_list = [(
-        datetime.datetime(date[0], date[1], date[2]),
-        # date[0],
-        # date[1],
-        # date[2],
-        calendar_service.DAYS_OF_WEEK[date[3]]
-    ) for date in month_calendar]
-    # print(calendar_date_list)
-    # for date in calendar_date_list:
-    #     print(date[0].date())
-    # get existing shift types
+
+    
+    # calendar_date_list is a list of dictionaries
+    # the keys are date_strings to make matching shifts easier
+    # ie; "2021-09-01": {"more keys": "more values"}
+    calendar_date_list = {}
+
+    # because month calendar year/month/day are numbers
+    # numbers less than 10 don't have preceeding 0's to match the formatting of
+    # the date strings, need to add 0's to the front of the numbers
+    for date in month_calendar:
+        year_string = f"{date[0]}"
+        month_string = f"{date[1]}"
+        day_string = f"{date[2]}"
+        if date[1] < 10:
+            month_string = f"0{date[1]}"
+        if date[2] < 10:
+            day_string = f"0{date[2]}"
+        date_dict = {
+            f"{year_string}-{month_string}-{day_string}": {
+                "date_string": f"{year_string}-{month_string}-{day_string}",
+                "day_of_week": calendar_service.DAYS_OF_WEEK[date[3]]
+            }
+        }
+        calendar_date_list.update(date_dict)
+
+
     shift_types = shift_type_repository.list_user_shift_types(
         db=db, user_id=current_user.id)
 
@@ -89,16 +101,13 @@ def get_add_shifts_page(
          "end_of_month": end_of_month}
     ).fetchall()
     user_shifts = []
-    for row in result:
-        # pprint(row._asdict())
+    for row in result:        
         user_shifts.append(row._asdict())
 
-    # need to be able to navigate months
-    # days of month should simply be listed
-    # don't need to group by week
-    # then each row should have buttons for each shift type
-    # click the button, add the shift
-    # this will feel way better on the phone
+    for shift in user_shifts:
+        key_to_find = f"{shift['date'].date()}"
+        if key_to_find in calendar_date_list:
+            calendar_date_list[f"{key_to_find}"]["shift"] = shift
 
     context = {
         "request": request,
@@ -112,6 +121,35 @@ def get_add_shifts_page(
     return block_templates.TemplateResponse(
         name="webapp/add-shifts/add-shifts-page.html",
         context=context
+    )
+
+
+@router.post("/add-shift-to-date", response_class=HTMLResponse)
+async def add_shift_to_date(
+    request: Request,
+    db: Annotated[Session, Depends(get_db)],
+):
+    if not auth_service.get_session_cookie(request.cookies):
+        return templates.TemplateResponse(
+            request=request,
+            name="website/web-home.html",
+            headers={"HX-Redirect": "/"},
+        )
+
+    current_user = auth_service.get_current_session_user(
+        db=db,
+        cookies=request.cookies)
+    # pprint(await request.form())
+
+    shift_types = shift_type_repository.list_user_shift_types(
+        db=db, user_id=current_user.id)
+
+    context = {"request": request, "user_data": current_user,
+               "shift_types": shift_types}
+    return block_templates.TemplateResponse(
+        name="webapp/add-shifts/add-shifts-page.html",
+        context=context,
+        block_name="list_form"
     )
 
 
