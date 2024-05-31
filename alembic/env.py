be8ff35d00e3b3be9618ca6fa69d3f810d58b948
Mark O'Connor <mark.oconnor14@gmail.com>
2024-05-31 22:16:35 +0800

    create DbChatMessage model and migrate

--- alembic/env.py
+++ alembic/env.py
@@ -13,7 +13,7 @@ from app.models.user_session_model import DBUserSession
 from app.models.db_shift_type import DbShiftType
 from app.models.db_shift import DbShift
 from app.models.share_model import DbShare
-from app.models.chat_models import DBChatRoom
+from app.models.chat_models import DBChatRoom, DBChatMessage
 
 # import app settings
 settings = get_settings()
--- app/models/chat_models.py
+++ app/models/chat_models.py
@@ -1,7 +1,7 @@
 
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, Boolean, DateTime, String, JSON
+from sqlalchemy import Column, Integer, Boolean, DateTime, String, JSON, Text
 from app.core.database import Base
 
 class DBChatRoom(Base):
@@ -31,3 +31,33 @@ class DBChatRoom(Base):
         default=None,
         onupdate=datetime.utcnow
     )
+
+
+class DBChatMessage(Base):
+    """
+    DB Model for chat messages
+    is_read tracks whether or not the receiver has read it. In this case, there will only ever be one receiver in the couple's chat.
+    """
+    __tablename__ = 'etime_chat_messages'
+
+    id = Column(Integer, primary_key=True)
+    room_id = Column(
+        String(length=255),
+        nullable=False,
+        index=True,
+    )
+    message = Column(Text, nullable=False)
+    sender_id = Column(Integer, nullable=False, index=True)
+    is_read = Column(Boolean, nullable=False, default=0)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=datetime.utcnow
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        nullable=True,
+        default=None,
+        onupdate=datetime.utcnow
+    )
+    
