7ce78200309f2556a7f71467741e49d12b54f1d1
Mark O'Connor <mark.oconnor14@gmail.com>
2024-07-24 17:03:04 +0800

    set get_user_unread messages to return 0 if db_chat_room does not exist

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -11,6 +11,9 @@ def get_user_unread_message_count(db: Session, current_user_id: int):
         DBChatRoom.chat_users.contains(current_user_id)
     ).first()
 
+    if not db_chat_room:
+        return 0
+
     db_chat_messages = db.query(DBChatMessage).filter(
         DBChatMessage.room_id == db_chat_room.room_id,
         DBChatMessage.is_read == 0,
