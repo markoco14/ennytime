a4695f99cad00bfe78dc9cf8067deb300ecd1a26
Mark O'Connor <mark.oconnor14@gmail.com>
2024-08-24 14:57:48 +0800

    add sender_id and receiver_id fields to DbShare model
    constraints not enforced and values allowed Null for first step

--- app/models/share_model.py
+++ app/models/share_model.py
@@ -1,6 +1,7 @@
 
 from sqlalchemy import Column, Integer, String, DateTime, UniqueConstraint
-from app.core.database import Base  # Make sure this import points to your Base declarative base instance
+# Make sure this import points to your Base declarative base instance
+from app.core.database import Base
 
 # change type to name later, easier to understand in the app
 
@@ -11,6 +12,8 @@ class DbShare(Base):
     id = Column(Integer, primary_key=True, index=True)
     owner_id = Column(Integer, index=True, nullable=False)
     guest_id = Column(Integer, index=True, nullable=False)
+    sender_id = Column(Integer, index=True, nullable=True)
+    receiver_id = Column(Integer, index=True, nullable=True)
 
     # Define the unique constraint within the table metadata
     __table_args__ = (
