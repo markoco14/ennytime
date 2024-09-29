e3d24c81bd3a3584825d56e8eb3725174163936d
Mark <mark.oconnor14@gmail.com>
2024-09-29 17:34:55 +0800

    restore post schedule and delete schedule routes

--- app/routers/scheduling_router.py
+++ app/routers/scheduling_router.py
@@ -155,3 +155,99 @@ def get_scheduling_index_page(
         name="scheduling/index.html",
         context=context
     )
+
+
+@router.post("/{date}/{type_id}", response_class=HTMLResponse)
+async def add_shift_to_date(
+    request: Request,
+    db: Annotated[Session, Depends(get_db)],
+    date: str,
+    type_id: int
+):
+    if not auth_service.get_session_cookie(request.cookies):
+        return templates.TemplateResponse(
+            request=request,
+            name="website/web-home.html",
+            headers={"HX-Redirect": "/"},
+        )
+
+    current_user = auth_service.get_current_session_user(
+        db=db,
+        cookies=request.cookies)
+
+    # check if shift already exists
+    # if exists delete, user will already have clicked a confirm on the frontend
+
+    date_segments = date.split("-")
+    db_shift = schemas.CreateShift(
+        type_id=type_id,
+        user_id=current_user.id,
+        date=datetime.datetime(int(date_segments[0]), int(
+            date_segments[1]), int(date_segments[2]))
+    )
+
+    new_shift = shift_repository.create_shift(db=db, shift=db_shift)
+
+    shift_type = shift_type_repository.get_user_shift_type(
+        db=db, user_id=current_user.id, shift_type_id=type_id)
+
+    context = {
+        "current_user": current_user,
+        "request": request,
+        "date": {"date_string": date},
+        "shifts": [new_shift],
+        "type": {"id": type_id, "long_name": shift_type.long_name, }
+    }
+
+    return block_templates.TemplateResponse(
+        name="/scheduling/schedule.html",
+        context=context,
+        block_name="shift_exists_button"
+    )
+
+
+@router.delete("/{date}/{type_id}", response_class=HTMLResponse)
+async def delete_shift_for_date(
+    request: Request,
+    db: Annotated[Session, Depends(get_db)],
+    date: str,
+    type_id: int
+):
+    if not auth_service.get_session_cookie(request.cookies):
+        return templates.TemplateResponse(
+            request=request,
+            name="website/web-home.html",
+            headers={"HX-Redirect": "/"},
+        )
+
+    current_user = auth_service.get_current_session_user(
+        db=db,
+        cookies=request.cookies)
+
+    # check if shift already exists
+    # if exists delete, user will already have clicked a confirm on the frontend
+    date_segments = date.split("-")
+    date_object = datetime.datetime(
+        int(date_segments[0]), int(date_segments[1]), int(date_segments[2]))
+
+    existing_shift = shift_repository.get_user_shift(
+        db=db, user_id=current_user.id, type_id=type_id, date_object=date_object)
+
+    if not existing_shift:
+        return Response(status_code=404)
+
+    shift_repository.delete_user_shift(db=db, shift_id=existing_shift.id)
+    shift_type = shift_type_repository.get_user_shift_type(
+        db=db, user_id=current_user.id, shift_type_id=type_id)
+    context = {
+        "current_user": current_user,
+        "request": request,
+        "date": {"date_string": date},
+        "type": {"id": type_id, "long_name": shift_type.long_name}
+    }
+
+    return block_templates.TemplateResponse(
+        name="scheduling/schedule.html",
+        context=context,
+        block_name="no_shift_button"
+    )
