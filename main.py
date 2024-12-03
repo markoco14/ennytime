fa490bedec1f708eab3f8f2e5fee1c73b9b4516b
Mark <mark.oconnor14@gmail.com>
2024-12-03 10:15:01 +0800

    set up '/' to redirect to '/calendar/year/month'
    to include year and month in the url path instead of as query parameters
    
    and switch to rendering calendar on '/calendar' page instead of conditional rendering on homepage

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """Main file to hold app and api routes"""
 import datetime
 from typing import Annotated, Optional
-from pprint import pprint
 import time
 from fastapi import Depends, FastAPI, Request, Form, Response
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 from mangum import Mangum
@@ -27,10 +26,6 @@ from app.routers import (
     scheduling_router,
     onboard_router
 )
-from app.services import calendar_service, calendar_shift_service, chat_service
-from app.models.user_model import DBUser
-from app.models.share_model import DbShare
-
 
 
 SETTINGS = get_settings()
@@ -118,95 +113,15 @@ def index(
     current_time = datetime.datetime.now()
     selected_year = year or current_time.year
     selected_month = month or current_time.month
-    selected_month_name = calendar_service.MONTHS[selected_month - 1]
-
-    # handle all birthday logic
-    birthdays = []
-    if current_user.has_birthday() and current_user.birthday_in_current_month(current_month=selected_month):
-        birthdays.append({
-            "name": current_user.display_name,
-            "day": current_user.birthday.day
-        })
-
-    # get user who shares their calendar with current user
-    # find the DbShare where current user id is the receiver_id 
-    bae_user = db.query(DBUser).join(DbShare, DBUser.id == DbShare.sender_id).filter(
-        DbShare.receiver_id == current_user.id).first()
-
-    if bae_user:
-        if bae_user.has_birthday() and bae_user.birthday_in_current_month(current_month=selected_month):
-            birthdays.append({
-                "name": bae_user.display_name,
-                "day": bae_user.birthday.day
-            })
-
-    # get previous and next month names for month navigation
-    prev_month_name, next_month_name = calendar_service.get_prev_and_next_month_names(
-        current_month=selected_month)
-
-    month_calendar = calendar_service.get_month_calendar(
-        year=selected_year, 
-        month=selected_month
-        )
-
-    month_calendar_dict = dict((str(day), {"date": str(
-        day), "day_number": day.day, "month_number": day.month, "shifts": [], "bae_shifts": []}) for day in month_calendar)
-
     
-    # gathering user ids to query shift table and get shifts for both users at once
-    user_ids = [current_user.id]
-    if bae_user:
-        user_ids.append(bae_user.id)
-
-    # get the start and end of the month for query filters
-    start_of_month = calendar_service.get_start_of_month(year=selected_year, month=selected_month)
-    end_of_month = calendar_service.get_end_of_month(year=selected_year, month=selected_month)
-
-    # get shifts for current user and bae user
-    all_shifts = calendar_shift_service.get_month_shift_info_for_users(
-        db=db,
-        user_ids=user_ids,
-        start_of_month=start_of_month,
-        end_of_month=end_of_month
-        )
-    
-    # update the calendar dictionary with sorted shifts
-    month_calendar_dict = calendar_shift_service.sort_shifts_by_user(all_shifts=all_shifts, month_calendar_dict=month_calendar_dict, current_user=current_user)
-
-    context = {
-        "request": request,
-        "current_user": current_user,
-        "birthdays": birthdays,
-        "selected_month": selected_month,
-        "selected_month_name": selected_month_name,
-        "selected_year": selected_year,
-        "prev_month_name": prev_month_name,
-        "next_month_name": next_month_name,
-        "month_calendar": list(month_calendar_dict.values()),
-        "days_of_week": calendar_service.DAYS_OF_WEEK,
-    }
-
+    # HX-Redirect required for hx-request
     if "hx-request" in request.headers:
-        response = templates.TemplateResponse(
-            name="calendar/fragments/calendar-oob.html",
-            context=context,
-        )
+        response = Response(status_code=301)
+        response.headers["HX-Redirect"] = f"/calendar/{selected_year}/{selected_month}"
         return response
-
-    # get chatroom id to link directly from the chat icon
-    # get unread message count so chat icon can display the count on page load
-    user_chat_data = chat_service.get_user_chat_data(
-        db=db,
-        current_user_id=current_user.id
-    )
-
-    context.update({"chat_data": user_chat_data})  
-    response = templates.TemplateResponse(
-        name="calendar/index.html",
-        context=context,
-    )
-
-    return response
+    
+    # Can use FastAPI Redirect with standard http request
+    return RedirectResponse(url=f"/calendar/{selected_year}/{selected_month}")
 
 
 @app.post("/search", response_class=HTMLResponse)
