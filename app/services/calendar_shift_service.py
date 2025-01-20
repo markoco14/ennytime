8f648b5a83bfbadc5061da03afd8221977916192
Mark <mark.oconnor14@gmail.com>
2025-01-20 09:25:05 +0800

    clean up query to get user shifts for month and order by user_id/date

--- app/services/calendar_shift_service.py
+++ app/services/calendar_shift_service.py
@@ -14,8 +14,14 @@ def get_shift_info_for_users(db: Session, user_ids: List[int]) -> List[Tuple[DbS
 
 def get_month_shift_info_for_users(db: Session, user_ids: List[int], start_of_month: datetime, end_of_month: datetime) -> List[Tuple[DbShift, DbShiftType]]:
     """Returns a list of shifts for the given users within the given month"""
-    return db.query(DbShift, DbShiftType).join(DbShiftType, DbShift.type_id == DbShiftType.id).filter(
-        DbShift.user_id.in_(user_ids)).filter(DbShift.date >= start_of_month).filter(DbShift.date <= end_of_month).all()
+    return db.query(DbShift, DbShiftType
+                    ).join(DbShiftType, DbShift.type_id == DbShiftType.id
+                    ).filter(DbShift.user_id.in_(user_ids)
+                    ).filter(DbShift.date >= start_of_month
+                    ).filter(DbShift.date <= end_of_month
+                    ).order_by(DbShift.user_id
+                    ).order_by(DbShift.date
+                    ).all()
 
 def sort_shifts_by_user(all_shifts: List[Tuple[DbShift, DbShiftType]], month_calendar_dict, current_user: DBUser) -> dict:
     """Takes a list of shifts and sorts them into a dictionary by date and user"""
