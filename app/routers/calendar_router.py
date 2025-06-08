ba64d8b1364b022f9c9c1db00a80ba8320b44742
Mark <mark.oconnor14@gmail.com>
2025-06-08 13:48:31 +0800

    restore endpoints removed that broke calendar day edit functionality

--- app/routers/calendar_router.py
+++ app/routers/calendar_router.py
@@ -61,3 +61,98 @@ def get_calendar_day_edit(
     day: int
 ):
     return handle_get_calendar_day_edit(request=request, current_user=current_user, year=year, month=month, day=day, db=db)
+
+
+@router.post("/calendar/card/{date_string}/edit/{shift_type_id}")
+def get_calendar_card_edit(
+    request: Request,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[DBUser, Depends(auth_service.user_dependency)],
+    date_string: str,
+    shift_type_id: int,
+):
+    if not current_user:
+        response = templates.TemplateResponse(
+            request=request,
+            name="website/web-home.html"
+        )
+        response.delete_cookie("session-id")
+
+        return response
+     # check if shift already exists
+    # if exists delete, user will already have clicked a confirm on the frontend
+
+    date_segments = date_string.split("-")
+    db_shift = schemas.CreateShift(
+        type_id=shift_type_id,
+        user_id=current_user.id,
+        date=datetime.datetime(int(date_segments[0]), int(
+            date_segments[1]), int(date_segments[2]))
+    )
+
+    new_shift = shift_repository.create_shift(db=db, shift=db_shift)
+
+    shift_type = shift_type_repository.get_user_shift_type(
+        db=db, user_id=current_user.id, shift_type_id=shift_type_id)
+
+    context = {
+        "current_user": current_user,
+        "request": request,
+        "date_string": date_string,
+        "shift_type": shift_type
+    }
+
+    return block_templates.TemplateResponse(
+        name="/calendar/fragments/edit-view.html",
+        context=context,
+        block_name="shift_exists_button"
+    )
+
+
+
+@router.delete("/calendar/card/{date_string}/edit/{shift_type_id}", response_class=HTMLResponse)
+async def delete_shift_for_date(
+    request: Request,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[DBUser, Depends(auth_service.user_dependency)],
+    date_string: str,
+    shift_type_id: int,
+):
+    if not auth_service.get_session_cookie(request.cookies):
+        return templates.TemplateResponse(
+            request=request,
+            name="website/web-home.html",
+            headers={"HX-Redirect": "/"},
+        )
+
+    current_user = auth_service.get_current_session_user(
+        db=db,
+        cookies=request.cookies)
+
+    # check if shift already exists
+    # if exists delete, user will already have clicked a confirm on the frontend
+    date_segments = date_string.split("-")
+    date_object = datetime.datetime(
+        int(date_segments[0]), int(date_segments[1]), int(date_segments[2]))
+
+    existing_shift = shift_repository.get_user_shift(
+        db=db, user_id=current_user.id, type_id=shift_type_id, date_object=date_object)
+
+    if not existing_shift:
+        return Response(status_code=404)
+
+    shift_repository.delete_user_shift(db=db, shift_id=existing_shift.id)
+    shift_type = shift_type_repository.get_user_shift_type(
+        db=db, user_id=current_user.id, shift_type_id=shift_type_id)
+    context = {
+        "current_user": current_user,
+        "request": request,
+        "date_string": date_string,
+        "shift_type": shift_type
+    }
+
+    return block_templates.TemplateResponse(
+        name="/calendar/fragments/edit-view.html",
+        context=context,
+        block_name="no_shift_button"
+    )
