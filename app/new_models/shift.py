16d854ed8ace9941d34109659315edea20541b4b
Mark <mark.oconnor14@gmail.com>
2025-10-12 10:32:38 +0800

    add get and delete methods to Shift domain model

--- app/new_models/shift.py
+++ app/new_models/shift.py
@@ -21,11 +21,36 @@ class Shift:
         return shifts_rows
     
     @classmethod
+    def get(cls, shift_id):
+        with sqlite3.connect("db.sqlite3") as conn:
+            conn.execute("PRAGMA foreign_keys = ON;")
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            cursor.execute("SELECT id, long_name, short_name, user_id FROM shifts WHERE id = ?;", (shift_id, ))
+            row = cursor.fetchone()
+            shift = Shift(
+                id=row["id"],
+                long_name=row["long_name"],
+                short_name=row["short_name"],
+                user_id=row["user_id"]
+            )
+            return shift
+    
+    @classmethod
     def create(cls, long_name: str, short_name: str, user_id: int):
         with sqlite3.connect("db.sqlite3") as conn:
             conn.execute("PRAGMA foreign_keys=ON;")
             cursor = conn.cursor()
             cursor.execute("INSERT INTO shifts (long_name, short_name, user_id) VALUES (?, ?, ?);", (long_name, short_name, user_id))
+            row_id = cursor.lastrowid
+
+        return row_id
+    
+    def delete(self):
+        with sqlite3.connect("db.sqlite3") as conn:
+            conn.execute("PRAGMA foreign_keys=ON;")
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM shifts WHERE id = ?;", (self.id, ))
             row_id = cursor.lastrowid
 
         return row_id
