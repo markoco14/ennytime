dc8c41b7712671ae88795fffca280c66895f400a
Mark <mark.oconnor14@gmail.com>
2025-12-23 09:39:16 +0800

    create holiday message table

---
+++ migrations/20251223_01_Qyxon-create-holiday-message-table.py
@@ -0,0 +1,22 @@
+"""
+Create holiday message table
+"""
+
+from yoyo import step
+
+__depends__ = {'20251221_01_QO2H0-create-holiday-table'}
+
+steps = [
+    step("""CREATE TABLE holiday_message (
+                message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                content TEXT NOT NULL,
+                holiday_id INTEGER NOT NULL,
+                sender_id INTEGER NOT NULL, 
+                recipient_id INTEGET NOT NULL,
+                FOREIGN KEY (holiday_id) REFERENCES holiday(holiday_id),
+                FOREIGN KEY (sender_id) REFERENCES users(id),
+                FOREIGN KEY (recipient_id) REFERENCES users(id)
+            );
+         """,
+         "DROP TABLE IF EXISTS holiday_message;")
+]
