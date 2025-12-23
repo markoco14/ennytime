19b22491eb5e3c5dc2e9357127716d48cbee7910
Mark <mark.oconnor14@gmail.com>
2025-12-23 10:08:05 +0800

    create holiday message table

--- migrations/20251223_01_Qyxon-create-holiday-message-table.py
+++ migrations/20251223_01_Qyxon-create-holiday-message-table.py
@@ -15,7 +15,8 @@ steps = [
                 recipient_id INTEGET NOT NULL,
                 FOREIGN KEY (holiday_id) REFERENCES holiday(holiday_id),
                 FOREIGN KEY (sender_id) REFERENCES users(id),
-                FOREIGN KEY (recipient_id) REFERENCES users(id)
+                FOREIGN KEY (recipient_id) REFERENCES users(id),
+                UNIQUE (holiday_id, sender_id, recipient_id)
             );
          """,
          "DROP TABLE IF EXISTS holiday_message;")
